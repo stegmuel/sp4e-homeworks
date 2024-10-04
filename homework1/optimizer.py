b1562e6658da20e48056acabe003ed3afe033e1e
stegmuel <thomas.stegmuller@epfl.ch>
2024-10-04 16:03:05 +0200

    Added typing and docstrings.

--- homework1/optimizer.py
+++ homework1/optimizer.py
@@ -3,9 +3,15 @@ from scipy.sparse.linalg import lgmres
 from scipy.optimize import minimize
 import argparse
 import matplotlib.pyplot as plt
+from numpy.typing import NDArray
+from typing import Callable, Optional, Tuple
 
 
 class MyCallback:
+    """
+    Class to collect intermediate results during optimization.
+    """
+
     def __init__(
         self,
     ):
@@ -15,7 +21,13 @@ class MyCallback:
         self.xs.append(x)
 
 
-def get_parser():
+def get_parser() -> argparse.Namespace:
+    """
+    Creates and returns an argument parser for the GMRES optimization methods.
+
+    Returns:
+        argparse.Namespace: A configured argument parser.
+    """
     parser = argparse.ArgumentParser(description="Args parser for GMRES")
     parser.add_argument(
         "--method",
@@ -27,8 +39,26 @@ def get_parser():
     return parser
 
 
-def get_function(matrix_a, vector_b):
-    def function(x):
+def get_function(matrix_a: NDArray, vector_b: NDArray) -> Callable[[NDArray], NDArray]:
+    """
+    Convenient way to get the optimization objective with A and b pre-set.
+    Args:
+        matrix_a (NDArray): A (m, m).
+        vector_b (NDArray): b (m,).
+
+    Returns:
+        Callable[[NDArray], NDArray]: Functor to compute the value of the objective at query points.
+    """
+
+    def function(x: NDArray) -> NDArray:
+        """
+        Computes the optimization objective.
+        Args:
+            x (NDArray): Query point.
+
+        Returns:
+            NDArray: Value the objective at input point(s).
+        """
         if x.ndim == 1:
             return np.einsum("m, m n, n -> ", x, matrix_a, x) - np.einsum(
                 "m, m ->", x, vector_b
@@ -47,7 +77,12 @@ def get_function(matrix_a, vector_b):
     return function
 
 
-def minimize_gmres(function=None, matrix_a=None, vector_b=None, method="lgmres"):
+def minimize_gmres(
+    function: Optional[Callable[[NDArray], NDArray]] = None,
+    matrix_a: Optional[NDArray] = None,
+    vector_b: Optional[NDArray] = None,
+    method: str = "lgmres",
+) -> Tuple[NDArray, MyCallback]:
     # Initialize the callback
     callback = MyCallback()
 
@@ -74,7 +109,13 @@ def minimize_gmres(function=None, matrix_a=None, vector_b=None, method="lgmres")
     )
 
 
-def plot_function(function, callback):
+def plot_function(function: Callable[[NDArray], NDArray], callback: MyCallback) -> None:
+    """Plot the function landscape and the intermediate optimization steps.
+
+    Args:
+        function (Callable[[NDArray], NDArray]): Functor to access the value of the objective at desired points.
+        callback (MyCallback): Object storing the intermediate optimization steps.
+    """
     # Get the intermediate points from the callback
     intermediate_xs = callback.xs
     intermediate_xs = np.stack(intermediate_xs, axis=0)
@@ -107,7 +148,7 @@ def plot_function(function, callback):
     plt.show()
 
 
-def main(args):
+def main(args: argparse.Namespace) -> None:
     # Define the matrix a
     matrix_a = np.array([[8, 1], [1, 3]], dtype=np.float32)
 
