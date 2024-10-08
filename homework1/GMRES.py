67395c20fbf08268d46659f7d8a9082c363bce6b
stegmuel <thomas.stegmuller@epfl.ch>
2024-10-08 17:33:32 +0200

    optimizer.py is the main script + black formatting

--- homework1/GMRES.py
+++ homework1/GMRES.py
@@ -1,4 +1,3 @@
-
 import argparse
 import numpy as np
 
@@ -10,47 +9,21 @@ from typing import Tuple
 from utils import ResultsSaverCallback
 
 
-def get_parser() -> argparse.Namespace :
-    """
-    Creates and returns an argument parser for the GMRES optimization methods.
-
-    Returns:
-        argparse.Namespace: A configured argument parser.
-    """
-    parser = argparse.ArgumentParser(description="Args parser for GMRES")
-    parser.add_argument(
-        "A",
-        type=list[float],
-        nargs="+",
-        help="Provide matrix A, should be a square matrix. Ex: 12 23"
-    )
-    parser.add_argument(
-        "b",
-        type=list[float],
-        help="Provide vector b, should be a vector of same size as A. Ex: 34"
-    )
-    parser.add_argument(
-        "-m",
-        "--method",
-        type=str,
-        choices=["lgmres2", "lgmres", "bfgs"],
-        default="lgmres2",
-        help="Method used to compute the minimizer.",
-    )
-    parser.add_argument(
-        "--plot",
-        action="store_true",
-        help="Whether to plot the optimization steps or not. Matrix A and vector b should have dimension 2 !"
-    )
-    return parser
-
-
-def gmres_(matrix_a : NDArray, vector_b : NDArray, tol : float = 1e-6, max_iters : int = 100)-> Tuple[NDArray, ResultsSaverCallback]:
+def gmres_(
+    matrix_a: NDArray,
+    vector_b: NDArray,
+    x_zero: NDArray,
+    callback: ResultsSaverCallback,
+    tol: float = 1e-6,
+    max_iters: int = 100,
+) -> Tuple[NDArray, ResultsSaverCallback]:
     """Solves linear equation Ax=b using the General Minimal Residual Method.
 
     Args:
         matrix_a (NDArray): The matrix A, an invertible square matrix of shape (n x n)
         vector_b (NDArray): The vector b of shape (n,)
+        x_zero (NDArray): Initialization for x of shape (n,)
+        callback (ResultsSaverCallback): Callable object to store intermediate points.
         tol (float, optional): Value under which the solution error is accepted. Defaults to 1e-6.
         max_iters (int, optional): Maximum number of iterations before returning a solution. Defaults to 100.
 
@@ -63,25 +36,22 @@ def gmres_(matrix_a : NDArray, vector_b : NDArray, tol : float = 1e-6, max_iters
     ), "A square matrix is expected."
     assert np.linalg.det(matrix_a) != 0, "The matrix is expected to be invertible."
 
-    def arnoldi_iteration(matrix_a : NDArray, matrix_q : NDArray, matrix_h : NDArray, i : int) -> Tuple[NDArray, NDArray]:
-        """Compute a basis of the (n + 1)-Krylov subspace of the matrix A used for GMRES optimization
-        """
+    def arnoldi_iteration(
+        matrix_a: NDArray, matrix_q: NDArray, matrix_h: NDArray, i: int
+    ) -> Tuple[NDArray, NDArray]:
+        """Compute a basis of the (n + 1)-Krylov subspace of the matrix A used for GMRES optimization"""
         # Arnoldi process to build the orthonormal basis V and Hessenberg matrix H
         q_k = np.einsum("m n, n -> m", matrix_a, matrix_q[:, i])
-        for j in range(i+1):
+        for j in range(i + 1):
             matrix_h[j, i] = np.einsum("m, m ->", matrix_q[:, j].conj(), q_k)
             q_k = q_k - matrix_h[j, i] * matrix_q[:, j]
-        
-        matrix_h[i+1, i] = norm(q_k)
-        matrix_q[:, i+1] = q_k / matrix_h[i+1, i]
+
+        matrix_h[i + 1, i] = norm(q_k)
+        matrix_q[:, i + 1] = q_k / matrix_h[i + 1, i]
         return matrix_h, matrix_q
 
     # Set default values
     n = len(vector_b)
-    x_zero = np.zeros_like(vector_b)
-
-    # Initialize callabck to save intermediate results
-    callback = ResultsSaverCallback(x_zero)
 
     # Normalize the right-hand side vector
     b_norm = norm(vector_b)
@@ -121,32 +91,3 @@ def gmres_(matrix_a : NDArray, vector_b : NDArray, tol : float = 1e-6, max_iters
         if error < tol:
             return x, callback
     return x, callback
-
-
-def main(args : argparse.Namespace) -> None:
-    # Define the matrix a
-    matrix_a = np.array(args.A, dtype=np.float32)
-
-    # Define the vector b
-    vector_b = np.array(args.b, dtype=np.float32)
-
-    # Get minimizer
-    if args.method == "lgmres2":
-        minimizer, callback = gmres_(matrix_a, vector_b)
-    else:
-        minimizer, callback = get_minimizer(matrix_a=matrix_a, vector_b=vector_b, method=args.method)
-    print(f"Solution x to Ax=b is: {minimizer}")
-
-    if args.plot:
-        assert (
-            len(vector_b) == 2
-        ), "Matrix A and vector b should have dimension 2 for plotting. Considere removing --plot option."
-        function = get_function(matrix_a=matrix_a, vector_b=vector_b)
-        plot_function(function, callback)
-
-
-if __name__ == "__main__":
-    # Parse the arguments
-    parser = get_parser()
-    args = parser.parse_args()
-    main(args)
--- homework1/optimizer.py
+++ homework1/optimizer.py
@@ -1,4 +1,3 @@
-
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,25 +6,41 @@ from numpy.typing import NDArray
 from scipy.sparse.linalg import lgmres
 from scipy.optimize import minimize
 from typing import Callable, Tuple
-
+from GMRES import gmres_
 from utils import ResultsSaverCallback
 
 
 def get_parser() -> argparse.Namespace:
     """
-    Creates and returns an argument parser for the GMRES optimization methods.
+    Creates and returns an argument parser for the optimization methods.
 
     Returns:
         argparse.Namespace: A configured argument parser.
     """
-    parser = argparse.ArgumentParser(description="Args parser for GMRES")
+    parser = argparse.ArgumentParser(description="Args parser for the optimization")
+    parser.add_argument(
+        "A",
+        type=list[float],
+        nargs="+",
+        help="Provide matrix A, should be a square matrix. Ex: 12 23",
+    )
+    parser.add_argument(
+        "b",
+        type=list[float],
+        help="Provide vector b, should be a vector of same size as A. Ex: 34",
+    )
     parser.add_argument(
         "-m",
         "--method",
         type=str,
-        choices=["lgmres", "bfgs"],
-        default="bfgs",
+        choices=["lgmres2", "lgmres", "bfgs"],
+        default="lgmres2",
         help="Method used to compute the minimizer.",
+    )
+    parser.add_argument(
+        "--plot",
+        action="store_true",
+        help="Whether to plot the optimization steps or not. Matrix A and vector b should have dimension 2 !",
     )
     return parser
 
@@ -51,17 +66,17 @@ def get_function(matrix_a: NDArray, vector_b: NDArray) -> Callable[[NDArray], ND
             NDArray: Value the objective at input point(s).
         """
         if x.ndim == 1:
-            return 0.5*np.einsum("m, m -> ", x, np.einsum("m n, n -> m ", matrix_a, x)) - np.einsum(
-                "m, m ->", x, vector_b
-            )
+            return 0.5 * np.einsum(
+                "m, m -> ", x, np.einsum("m n, n -> m ", matrix_a, x)
+            ) - np.einsum("m, m ->", x, vector_b)
         elif x.ndim == 2:
-            return 0.5*np.einsum("b m, b m -> b", x,  np.einsum("m n, b n -> b m", matrix_a, x)) - np.einsum(
-                "b m, m -> b", x, vector_b
-            )
+            return 0.5 * np.einsum(
+                "b m, b m -> b", x, np.einsum("m n, b n -> b m", matrix_a, x)
+            ) - np.einsum("b m, m -> b", x, vector_b)
         elif x.ndim == 3:
-            return 0.5*np.einsum("b c m, b c m -> b c", x, np.einsum("m n, b c n -> b c m", matrix_a, x)) - np.einsum(
-                "b c m, m -> b c", x, vector_b
-            )
+            return 0.5 * np.einsum(
+                "b c m, b c m -> b c", x, np.einsum("m n, b c n -> b c m", matrix_a, x)
+            ) - np.einsum("b c m, m -> b c", x, vector_b)
         else:
             raise NotImplementedError
 
@@ -86,34 +101,39 @@ def get_minimizer(
     Returns:
         Tuple[NDArray, ResultsSaverCallback]: Returns the minimizer x and the intermediate steps.
     """
-    
-    # Initialize starting point to origin
-    x0 = np.zeros_like(vector_b, dtype=np.float32)
+
+    # Initialize starting point
+    x_zero = np.zeros_like(vector_b)
 
     # Initialize the callback
-    callback = ResultsSaverCallback(x0)
+    callback = ResultsSaverCallback(x_zero)
 
     # Ensure matrix a and vector b are provided when the method is lgmres
-    if method == "lgmres":
+    if method in {"lgmres", "lgmres2"}:
         assert (
             matrix_a is not None and vector_b is not None
         ), "Matrix a vector b can not be none when using lgmres."
-        return (lgmres(matrix_a, vector_b, x0=x0, callback=callback)[0], callback)
-    
+        if method == "lgmres":
+            return (
+                lgmres(matrix_a, vector_b, x0=x_zero, callback=callback)[0],
+                callback,
+            )
+        return gmres_(matrix_a, vector_b, x_zero, callback)
+
     elif method == "bfgs":
-        function=get_function(matrix_a=matrix_a, vector_b=vector_b)
+        function = get_function(matrix_a=matrix_a, vector_b=vector_b)
         return (
-            minimize(
-                fun=function, x0=x0, method="BFGS", callback=callback
-            ).x,
+            minimize(fun=function, x0=x_zero, method="BFGS", callback=callback).x,
             callback,
         )
-    
+
     else:
         raise NotImplementedError(f"Optimization method {method} not implemented !")
 
 
-def plot_function(function: Callable[[NDArray], NDArray], callback: ResultsSaverCallback) -> None:
+def plot_function(
+    function: Callable[[NDArray], NDArray], callback: ResultsSaverCallback
+) -> None:
     """Plot the function landscape and the intermediate optimization steps.
 
     Args:
@@ -139,12 +159,12 @@ def plot_function(function: Callable[[NDArray], NDArray], callback: ResultsSaver
     # Plot the surface
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    ax.view_init(elev=30., azim=135, roll=0)
+    ax.view_init(elev=30.0, azim=135, roll=0)
     ax.plot_surface(x_grid, y_grid, s_xs, alpha=0.3, cmap="viridis")
     ax.contour3D(x_grid, y_grid, s_xs, 10, colors="black")
-    ax.set_xlabel(r'$x_1$')
-    ax.set_ylabel(r'$x_2$')
-    ax.set_zlabel(r'$s(x_1,x_2)$')
+    ax.set_xlabel(r"$x_1$")
+    ax.set_ylabel(r"$x_2$")
+    ax.set_zlabel(r"$s(x_1,x_2)$")
     ax.plot(
         intermediate_xs[:, 0],
         intermediate_xs[:, 1],
@@ -154,7 +174,7 @@ def plot_function(function: Callable[[NDArray], NDArray], callback: ResultsSaver
         linewidth=1,
         linestyle="dashed",
         markersize=4,
-        alpha=1.
+        alpha=1.0,
     )
     plt.show()
 
@@ -176,7 +196,12 @@ def main(args: argparse.Namespace) -> None:
     function = get_function(matrix_a=matrix_a, vector_b=vector_b)
 
     # Plot the function
-    plot_function(function, callback)
+    if args.plot:
+        assert (
+            len(vector_b) == 2
+        ), "Matrix A and vector b should have dimension 2 for plotting. Considere removing --plot option."
+        function = get_function(matrix_a=matrix_a, vector_b=vector_b)
+        plot_function(function, callback)
 
 
 if __name__ == "__main__":
