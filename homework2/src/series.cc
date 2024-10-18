0fc7f4ee685e2201a009cf032e34459228e34ce0
stegmuel <thomas.stegmuller@epfl.ch>
2024-10-18 10:07:40 +0200

    analytic prediction for series

---
+++ homework2/src/series.cc
@@ -0,0 +1,7 @@
+#include "series.hh"
+#include <cmath>
+
+double Series::getAnalyticPrediction()
+{
+    return std::nan("");
+}
--- homework2/src/series.hh
+++ homework2/src/series.hh
@@ -1,8 +1,9 @@
 #ifndef SERIES_HH
-#define  SERIES_HH
+#define SERIES_HH
 class Series
 {
 public:
     virtual double compute(unsigned int N) = 0;
+    virtual double getAnalyticPrediction();
 };
 #endif
