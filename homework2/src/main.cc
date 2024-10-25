1468406a4a1d9b596f3c1a23530a7cee7498ff4c
stegmuel <thomas.stegmuller@epfl.ch>
2024-10-25 16:24:36 +0200

    Dumping to an output filestream object

--- homework2/src/main.cc
+++ homework2/src/main.cc
@@ -16,6 +16,8 @@
 #include "print_series.hh"
 #include "write_series.hh"
 /* -------------------------------------------------------------------------- */
+// TODO: python script to plot values
+/* -------------------------------------------------------------------------- */
 
 ArgParser getParser()
 {
@@ -26,10 +28,11 @@ ArgParser getParser()
     parser.addArgument("integral_f", "cubic", {"cubic", "cos", "sin"});
     parser.addArgument("integral_a", "0");
     parser.addArgument("integral_b", "1");
-    parser.addArgument("dumper_type", "write", {"write", "dump"});
+    parser.addArgument("dumper_type", "write", {"write", "print"});
     parser.addArgument("separator_type", ",", {",", " ", "  ", "|"});
     parser.addArgument("maxiter", "100");
     parser.addArgument("print_frequency", "10");
+    parser.addArgument("precision", "6");
 
     return parser;
 }
@@ -60,88 +63,97 @@ float get_bounds(const std::string bound, std::string bound_name)
 }
 
 int main(int argc, char *argv[])
+{
+    // Get the parser with default values
+    ArgParser parser = getParser();
+
+    // Parse the arguments
+    parser.parse(argc, argv);
+
+    // Identify the desired serie class
+    std::string serie_type = parser.get("serie_type");
+
+    // Instantiate the desired serie class
+    std::shared_ptr<Series> serie = nullptr;
+    if (serie_type == "pi")
     {
-        // Get the parser with default values
-        ArgParser parser = getParser();
+        // Instantiate a PI serie
+        serie = std::make_shared<ComputePI>();
+    }
+    else if (serie_type == "arithmetic")
+    {
+        // Instantiate an arithmetic serie
+        serie = std::make_shared<ComputeArithmetic>();
+    }
+    else
+    {
+        // Set the function
+        std::function<double(double)> f;
+        std::string function = parser.get("integral_f");
 
-        // Parse the arguments
-        parser.parse(argc, argv);
-
-        // Identify the desired serie class
-        std::string serie_type = parser.get("serie_type");
-
-        // Instantiate the desired serie class
-        std::shared_ptr<Series> serie = nullptr;
-        if (serie_type == "pi")
+        // Set the integral bounds
+        float a = get_bounds(parser.get("integral_a"), "a");
+        float b = get_bounds(parser.get("integral_b"), "b");
+        if (function == "cubic")
         {
-            // Instantiate a PI serie
-            serie = std::make_shared<ComputePI>();
+            f = [](double x)
+            { return x * x * x; };
         }
-        else if (serie_type == "arithmetic")
+        else if (function == "cos")
         {
-            // Instantiate an arithmetic serie
-            serie = std::make_shared<ComputeArithmetic>();
+            f = [](double x)
+            { return cos(x); };
+        }
+        else if (function == "sin")
+        {
+            f = [](double x)
+            { return sin(x); };
         }
         else
         {
-            // Set the function
-            std::function<double(double)> f;
-            std::string function = parser.get("integral_f");
-
-            // Set the integral bounds
-            float a = get_bounds(parser.get("integral_a"), "a");
-            float b = get_bounds(parser.get("integral_b"), "b");
-            if (function == "cubic")
-            {
-                f = [](double x)
-                { return x * x * x; };
-            }
-            else if (function == "cos")
-            {
-                f = [](double x)
-                { return cos(x); };
-            }
-            else if (function == "sin")
-            {
-                f = [](double x)
-                { return sin(x); };
-            }
-            else
-            {
-                std::cerr << "The function to integrate must be either of cubic, cos or sin." << std::endl;
-                return EXIT_FAILURE;
-            }
-            serie = std::make_shared<RiemannIntegral>(a, b, f);
-        }
-
-        // Identify the desired dumper class
-        std::string dumper_type = parser.get("dumper_type");
-
-        // Instantiate the desired dumper class
-        std::shared_ptr<DumperSeries> dumper = nullptr;
-
-        // Get dumper-related arguments
-        int maxiter = std::stoi(parser.get("maxiter"));
-        int print_frequency = std::stoi(parser.get("print_frequency"));
-        std::string separator = parser.get("separator");
-
-        if (dumper_type == "print"){
-            dumper = std::make_shared<PrintSeries>(*serie, maxiter, print_frequency);
-        } else if (dumper_type == "write"){
-            dumper = std::make_shared<WriteSeries>(*serie, maxiter, print_frequency);
-            dumper->setSeparator(separator);
-        }else{
-            std::cerr << "Options should be read or write." << std::endl;
+            std::cerr << "The function to integrate must be either of cubic, cos or sin." << std::endl;
             return EXIT_FAILURE;
         }
-
-        // Dump the result
-        dumper->dump();
-
-        // Compute the serie and print
-        double serie_result;
-        serie_result = serie->compute(100);
-        std::cout << serie_result << std::endl;
-
-        return EXIT_SUCCESS;
+        serie = std::make_shared<RiemannIntegral>(a, b, f);
     }
+
+    // Identify the desired dumper class
+    std::string dumper_type = parser.get("dumper_type");
+
+    // Instantiate the desired dumper class
+    std::shared_ptr<DumperSeries> dumper = nullptr;
+
+    // Get dumper-related arguments
+    int maxiter = std::stoi(parser.get("maxiter"));
+    int print_frequency = std::stoi(parser.get("print_frequency"));
+    std::string separator = parser.get("separator");
+
+    // Set the output
+    std::shared_ptr<std::ostream> ofstream = nullptr;
+
+    // Instantiate the dumper and the output stream
+    if (dumper_type == "print")
+    {
+        dumper = std::make_shared<PrintSeries>(*serie, maxiter, print_frequency);
+        ofstream = std::shared_ptr<std::ostream>(&std::cout, [](std::ostream *) {});
+    }
+    else if (dumper_type == "write")
+    {
+        dumper = std::make_shared<WriteSeries>(*serie, maxiter, print_frequency);
+        ofstream = std::make_shared<std::ofstream>("test.txt");
+    }
+    else
+    {
+        std::cerr << "Options should be read or write." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Set the precision
+    dumper->setPrecision(std::stoi(parser.get("precision")));
+    dumper->setSeparator(separator);
+
+    // Dump the result
+    *ofstream << *dumper;
+
+    return EXIT_SUCCESS;
+}
