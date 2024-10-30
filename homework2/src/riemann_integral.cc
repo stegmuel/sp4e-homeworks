0c8267e8f6e9f69794da623add6211605937151a
GuillaumeVray <guillaume.vray@epfl.ch>
2024-10-30 10:30:00 +0100

    Correct error related to Riemann integral

--- homework2/src/riemann_integral.cc
+++ homework2/src/riemann_integral.cc
@@ -6,9 +6,8 @@ double RiemannIntegral::compute(unsigned int N)
 {
     double sum = 0.0;
     double delta_x = (this->b - this->a) / N;
-    double x_i = this->a;
     for (int i=1; i<=N; i++){
-        x_i += delta_x;
+        double x_i = this->a + i*delta_x;
         sum += delta_x * this->f(x_i);
     }
     return sum;
