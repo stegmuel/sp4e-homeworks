f9a75f40e890bb80c4dab42f4f22289bd74178d7
GuillaumeVray <guillaume.vray@epfl.ch>
2024-12-01 02:18:59 +0100

    Add fft forward (test passed) + fft backward and compute frequencies (to be tested)

--- homework3/src/fft.hh
+++ homework3/src/fft.hh
@@ -16,14 +16,64 @@ struct FFT {
 
 /* ------------------------------------------------------ */
 
-inline Matrix<complex> FFT::transform(Matrix<complex>& m_in) {}
+inline Matrix<complex> FFT::transform(Matrix<complex>& m_in) {
+  // Get Matrix dimension
+  UInt n0 = m_in.rows();
+  UInt n1 = m_in.cols();
+
+  // convert m_in array to fftw_complex type
+  fftw_complex* in = reinterpret_cast<fftw_complex*>(m_in.data());
+
+  // Init m_out
+  Matrix<complex> m_out = Matrix<complex>(n0);
+  fftw_complex* out = reinterpret_cast<fftw_complex*>(m_out.data());
+  
+  // Compute plan in forward mode and execute
+  fftw_plan p = fftw_plan_dft_2d(n0, n1, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+  fftw_execute(p);
+  fftw_destroy_plan(p);
+  return m_out;
+}
 
 /* ------------------------------------------------------ */
 
-inline Matrix<complex> FFT::itransform(Matrix<complex>& m_in) {}
+inline Matrix<complex> FFT::itransform(Matrix<complex>& m_in) {
+  // Get Matrix dimension
+  UInt n0 = m_in.rows();
+  UInt n1 = m_in.cols();
+
+  // convert m_in array to fftw_complex type
+  fftw_complex* in = reinterpret_cast<fftw_complex*>(m_in.data());
+
+  // Init m_out
+  Matrix<complex> m_out = Matrix<complex>(n0);
+  fftw_complex* out = reinterpret_cast<fftw_complex*>(m_out.data());
+  
+  // Compute plan in backward mode and execute
+  fftw_plan p = fftw_plan_dft_2d(n0, n1, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+  fftw_execute(p);
+  m_out /= (m_in.rows() * m_in.cols());
+  fftw_destroy_plan(p);
+  return m_out;
+}
 
 /* ------------------------------------------------------ */
 
-inline Matrix<std::complex<int>> FFT::computeFrequencies(int size) {}
+inline Matrix<std::complex<int>> FFT::computeFrequencies(int size) {
+  Matrix<std::complex<int>> m_out = Matrix<std::complex<int>>(size);
+  int N = ((size-1)/2) + 1;
+
+  for (auto&& cell: index(m_out)){
+    UInt i = std::get<0>(cell);
+    UInt j = std::get<1>(cell);
+    std::complex<int>& value = std::get<2>(cell);
+
+    int freq_r = i < N ? i : i - size;
+    int freq_i = j < N ? j : j - size;
+    value.real(freq_r);
+    value.imag(freq_i);
+  }
+  return m_out;
+}
 
 #endif  // FFT_HH
