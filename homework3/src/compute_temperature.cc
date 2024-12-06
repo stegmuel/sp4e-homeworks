38678aecac8d3bffb553ac75cf574b3c9e2e6185
GuillaumeVray <guillaume.vray@epfl.ch>
2024-12-06 14:28:30 +0100

    passing sine test but not two lines test

--- homework3/src/compute_temperature.cc
+++ homework3/src/compute_temperature.cc
@@ -8,51 +8,42 @@
 
 void ComputeTemperature::compute(System& system) {
     // We assume particles are aranged in a N*N grid with N>=2
-    UInt size = sqrt(system.getNbParticles());
-    double delta = size*(system.getParticle(1).getPosition()[0] - system.getParticle(0).getPosition()[0]);
-    Matrix<complex> theta_grid(size);
-    Matrix<complex> hv_grid(size);
+    UInt grid_size = sqrt(system.getNbParticles());
+    Real delta = grid_size*(system.getParticle(1).getPosition()[0] - system.getParticle(0).getPosition()[0]);
+    Matrix<complex> theta_grid(grid_size);
+    Matrix<complex> hv_grid(grid_size);
 
     // Fill temperature and volumetric heat source grid
-    int idx = 0;
-    for (auto& part : system){
-        int i = idx % size;
-        int j = idx / size;
-        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(part);
-        theta_grid(i,j).real(mp.getTemperature());
-        theta_grid(i,j).imag(0.);
-        hv_grid(i,j).real(mp.getHeatRate());
-        hv_grid(i,j).imag(0.);
-        idx += 1;
+    for (UInt j=0; j < grid_size; ++j){
+        for (UInt i=0; i < grid_size; ++i){
+            MaterialPoint& mp = dynamic_cast<MaterialPoint&>(system.getParticle(j*grid_size+i));
+            theta_grid(i,j) = mp.getTemperature();
+            hv_grid(i,j) = mp.getHeatRate();
+        }
     }
 
     // Compute fourier transforms of temperature and volumetric heat source grids
     Matrix<complex> fft_theta_grid = FFT::transform(theta_grid);
     Matrix<complex> fft_hv_grid = FFT::transform(hv_grid);
-    Matrix<std::complex<int>> fft_freqs = FFT::computeFrequencies(size);
+    Matrix<std::complex<int>> fft_freqs = FFT::computeFrequencies(grid_size);
 
     // Compute derivative of fourier theta grid
-    Real rho = 1.;
-    Real capacity = 1.;
-    Real kappa = 1.;
-    Matrix<complex> der_fft_theta_grid(size);
+    Matrix<complex> der_fft_theta_grid(grid_size);
     for (auto&& entry : index(der_fft_theta_grid)){
         int i = std::get<0>(entry);
         int j = std::get<1>(entry);
         auto& val = std::get<2>(entry);
-        val = (1./(rho*capacity))*(fft_hv_grid(i,j) - fft_theta_grid(i,j)*(pow(fft_freqs(i,j).real()/delta,2) + pow(fft_freqs(i,j).imag()/delta,2)));
+        Real q2x_q2y = Real(pow(fft_freqs(i,j).real(),2) + pow(fft_freqs(i,j).imag(),2));
+        val = fft_hv_grid(i,j) - fft_theta_grid(i,j)*(q2x_q2y)*pow(2*M_PI/delta,2);
     }
 
     // We update temperature of the system. We assume delta T is 1.
-    Real delta_t = 1.;
     Matrix<complex> der_theta_grid = FFT::itransform(der_fft_theta_grid);
-    idx = 0.;
-    for (auto& part : system){
-        int i = idx % size;
-        int j = idx / size;
-        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(part);
-        mp.getTemperature() += std::real(der_theta_grid(i,j));
-        idx+=1;
+    for (UInt j=0; j < grid_size; ++j){
+        for (UInt i=0; i < grid_size; ++i){
+            MaterialPoint& mp = dynamic_cast<MaterialPoint&>(system.getParticle(j*grid_size+i));
+            mp.getTemperature() += std::real(der_theta_grid(i,j));
+        }
     }
 }
 
--- homework3/src/test_temperature.cc
+++ homework3/src/test_temperature.cc
@@ -9,18 +9,21 @@
 #include "vector.hh"
 
 
-class ComputeTemperatureHomogeneous : public ::testing::Test {
+class RandomMaterialPoints : public ::testing::Test {
 protected:
   void SetUp() override {
     MaterialPointsFactory::getInstance();
     std::vector<MaterialPoint> material_points;
-    n_points = 9;
-    for (UInt i = 0; i < n_points; ++i) {
-      MaterialPoint p;
-      p.getPosition() = i;
-      p.getTemperature() = 1.;
-      p.getHeatRate() = 0.;
-      material_points.push_back(p);
+    grid_size = 9;
+    for (UInt i = 0; i < grid_size; ++i) {
+      for (UInt j = 0; j < grid_size; ++j){
+        MaterialPoint p;
+        p.getPosition()[0] = Real(j);
+        p.getPosition()[1] = Real(i);
+        p.getTemperature() = 1.;
+        p.getHeatRate() = 0.;
+        material_points.push_back(p);
+      }
     }
 
     for (auto& p : material_points) {
@@ -30,17 +33,107 @@ protected:
   }
 
   System system;
-  UInt n_points;
+  UInt grid_size;
+  ComputeTemperature compute_temperature = ComputeTemperature();
 };
 
 
-TEST_F(ComputeTemperatureHomogeneous, homogeneous_temperature) {
-    ComputeTemperature ct = ComputeTemperature();
-    ct.compute(system);
+TEST_F(RandomMaterialPoints, homogeneous_temperature) {
+  // Perform one step of integration
+  compute_temperature.compute(system);
 
-    for (auto& p: system) {
-        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(p);
-        ASSERT_NEAR(mp.getTemperature(), 1., 1e-10);
-    }
+  // Verify that the temperature is unchanged
+  for (auto& p: system) {
+    MaterialPoint& mp = dynamic_cast<MaterialPoint&>(p);
+    ASSERT_NEAR(mp.getTemperature(), 1., 1e-10);
+  }
+}
 
+
+TEST_F(RandomMaterialPoints, sine_temperature) {
+  // Set the grid attributes
+  Real x_min = -1.;
+  Real x_max = 1.;
+  Real y_min = -1.;
+  Real y_max = 1.;
+  Real L = x_max - x_min;
+  Real delta = L / grid_size;
+
+  // Iterate over each point and set the volumetric heat source
+  for (UInt i = 0; i < grid_size; ++i) {
+      for (UInt j = 0; j < grid_size; ++j){
+        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(system.getParticle(j*grid_size+i));
+        mp.getPosition()[0] = x_min + i * delta;
+        mp.getPosition()[1] = y_min + j * delta;
+        mp.getHeatRate() = pow(2*M_PI/L, 2)*sin(2*M_PI/L * mp.getPosition()[0]);
+        mp.getTemperature() = sin(2*M_PI/L * mp.getPosition()[0]);
+      }
+  }
+
+  // Check that the result is stable after one step
+  compute_temperature.compute(system);
+
+  // Iteratr over each material point and verify that the temperature matches the analytical prediction
+  for (UInt i = 0; i < grid_size; ++i) {
+      for (UInt j = 0; j < grid_size; ++j){
+        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(system.getParticle(j*grid_size+i));
+        ASSERT_NEAR(mp.getTemperature(), sin(2*M_PI/L * mp.getPosition()[0]), 1e-3);
+      }
+  }
+}
+
+
+TEST_F(RandomMaterialPoints, twolines_temperature) {
+  // Set the grid attributes
+  Real x_min = -1.;
+  Real x_max = 1.;
+  Real y_min = -1.;
+  Real y_max = 1.;
+  Real L = x_max - x_min;
+  Real delta = L / (grid_size-1);
+  
+  // Iterate over each point and set the volumetric heat source
+  for (UInt i = 0; i < grid_size; ++i) {
+      for (UInt j = 0; j < grid_size; ++j){
+        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(system.getParticle(j*grid_size+i));
+        mp.getPosition()[0] = x_min + i * delta;
+        mp.getPosition()[1] = y_min + j * delta;
+
+        // Set Heat Rate
+        if (mp.getPosition()[0] == 0.5){
+          mp.getHeatRate() = 1.;
+        }else if(mp.getPosition()[0] == -0.5){
+          mp.getHeatRate() = -1.;
+        }else{
+          mp.getHeatRate() = 0.;
+        }
+
+        // Set Temperature
+        if (mp.getPosition()[0] <= -0.5){
+          mp.getTemperature() = -mp.getPosition()[0]-1.;
+        }else if(mp.getPosition()[0] <= 0.5){
+          mp.getTemperature() = mp.getPosition()[0]*1.;
+        }else{
+          mp.getTemperature() = -mp.getPosition()[0]+1.;
+        }
+      }
+  }
+
+  // Check that the result is stable after one step
+  compute_temperature.compute(system);
+
+  // Iteratr over each material point and verify that the temperature matches the analytical prediction
+  for (UInt i = 0; i < grid_size; ++i) {
+      for (UInt j = 0; j < grid_size; ++j){
+        MaterialPoint& mp = dynamic_cast<MaterialPoint&>(system.getParticle(j*grid_size+i));
+        if (mp.getPosition()[0] <= -0.5){
+          ASSERT_NEAR(mp.getTemperature(), -mp.getPosition()[0]-1., 1e-10);
+        }else if(mp.getPosition()[0] <= 0.5){
+          ASSERT_NEAR(mp.getTemperature(), mp.getPosition()[0]*1., 1e-10);
+        }else{
+          ASSERT_NEAR(mp.getTemperature(), -mp.getPosition()[0]+1., 1e-10);
+        }
+        
+      }
+  }
 }
