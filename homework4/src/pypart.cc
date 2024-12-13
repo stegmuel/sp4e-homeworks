48d86754c56ba54c4de8151e3021b3cb90773bf7
stegmuel <thomas.stegmuller@epfl.ch>
2024-12-13 17:53:17 +0100

    Added all classes (without methods) to pypart.cc

--- homework4/src/pypart.cc
+++ homework4/src/pypart.cc
@@ -2,6 +2,7 @@
 
 namespace py = pybind11;
 
+#include "compute.hh"
 #include "compute_gravity.hh"
 #include "compute_temperature.hh"
 #include "compute_verlet_integration.hh"
@@ -22,22 +23,53 @@ PYBIND11_MODULE(pypart, m) {
   );
 	 
   // MaterialPointsFactory
-  py::class_<MaterialPointsFactory>(
+  py::class_<MaterialPointsFactory, ParticlesFactoryInterface>(
       m, "MaterialPointsFactory",
       py::dynamic_attr() // to allow new members to be created dynamically
   );
 
   // PingPongBallsFactory
-  py::class_<PingPongBallsFactory>(
+  py::class_<PingPongBallsFactory, ParticlesFactoryInterface>(
       m, "PingPongBallsFactory",
       py::dynamic_attr() // to allow new members to be created dynamically
   );
 
   // PlanetsFactory
-  py::class_<PlanetsFactory>(
+  py::class_<PlanetsFactory, ParticlesFactoryInterface>(
       m, "PlanetsFactory",
       py::dynamic_attr() // to allow new members to be created dynamically
   );
 
 	////////////// CSV ///////////////////
+  // CsvWriter
+  py::class_<CsvWriter>(
+      m, "CsvWriter",
+      py::dynamic_attr() // to allow new members to be created dynamically
+  );
+
+	////////////// Computes ///////////////////
+  // Compute
+  py::class_<Compute>(
+      m, "Compute",
+      py::dynamic_attr() // to allow new members to be created dynamically
+  );
+
+  // ComputeTemperature
+  py::class_<ComputeTemperature, Compute>(
+      m, "ComputeTemperature",
+      py::dynamic_attr() // to allow new members to be created dynamically
+  );
+
+  // ComputeGravity
+  py::class_<ComputeGravity, Compute>(
+      m, "ComputeGravity",
+      py::dynamic_attr() // to allow new members to be created dynamically
+  );
+
+  // ComputeVerletIntegration
+  py::class_<ComputeVerletIntegration, Compute>(
+      m, "ComputeVerletIntegration",
+      py::dynamic_attr() // to allow new members to be created dynamically
+  );
+
 }
